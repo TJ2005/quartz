4e90196a6ec7c7373b22dc9d42d90bf8324c6053
tj2005 <tejas.kamal.sahu@gmail.com>
2025-10-04 09:35:55 +0530

    hello

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Tejas TJ25's Notes",
+    pageTitle: "Tejas Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
