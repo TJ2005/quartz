ed1f8321772f360b8f3f5d54ce3618b2d930ed4e
tj2005 <tejas.kamal.sahu@gmail.com>
2025-10-04 08:57:38 +0530

    incorrect workflow path corrected

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "(Tejas) TJ25's Notes",
+    pageTitle: "TJ25's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
