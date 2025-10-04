11d24e1c128dc23507e29ca229a931be0104f9ba
tj2005 <tejas.kamal.sahu@gmail.com>
2025-10-04 08:55:33 +0530

    testing thhe new workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "TJ25's Notes",
+    pageTitle: "(Tejas) TJ25's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
